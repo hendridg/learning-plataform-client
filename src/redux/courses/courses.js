4a26ad78d034d24f2ef7ad649d1229ae610edd24
hendridg <hendridg@gmail.com>
2022-03-18 19:06:09 -0400

    Run linters

--- src/redux/courses/courses.js
+++ src/redux/courses/courses.js
@@ -1,65 +1,44 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  statusCallApiCourses: "idle",
-  courses: [
-    // {
-    //   id: 1,
-    //   name: "Math",
-    //   date: "2022-03-05",
-    //   hour: "9:00-10:00",
-    //   tutor: "Clint Eastwood",
-    //   tutor_id: "1223442",
-    // },
-    // {
-    //   id: 2,
-    //   name: "Phy",
-    //   date: "2022-03-05",
-    //   hour: "9:00-10:00",
-    //   tutor: "Stephen Kind",
-    //   tutor_id: "12ju234442",
-    // },
-  ],
+  statusCallApiCourses: 'idle',
+  courses: [],
 };
 
 export const fetchApiCourses = createAsyncThunk(
-  "courses/fetchApiCourses",
+  'courses/fetchApiCourses',
   async () => {
-    const response = await fetch("http://localhost:8000/courses").then((res) =>
-      res.json()
-    );
+    const response = await fetch('http://localhost:8000/courses').then((res) => res.json());
     return response;
-  }
+  },
 );
 
 const coursesSlice = createSlice({
-  name: "courses",
+  name: 'courses',
   initialState,
   reducers: {
-    addCourse: (state, action) => {
-      return {
-        ...state,
-        courses: [...state.courses, action.payload],
-      };
-    },
+    addCourse: (state, action) => ({
+      ...state,
+      courses: [...state.courses, action.payload],
+    }),
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchApiCourses.pending, (state) => ({
         ...state,
-        statusCallApiCourses: "loading",
+        statusCallApiCourses: 'loading',
       }))
       .addCase(fetchApiCourses.fulfilled, (state, action) => {
         const arrayCourses = action.payload.map((course) => ({
           id: course.id,
           name: course.name,
           date: course.date,
-          flickrImg: course.hour,
+          hour: course.hour,
           description: course.description,
         }));
         return {
           ...state,
-          statusCallApiCourses: "done",
+          statusCallApiCourses: 'done',
           courses: arrayCourses,
         };
       });
@@ -68,7 +47,6 @@ const coursesSlice = createSlice({
 
 export const { addCourse } = coursesSlice.actions;
 export const selectCourses = (state) => state.courses.courses;
-export const selectStatusCallApiCourses = (state) =>
-  state.courses.selectStatusCallApiCourses;
+export const selectStatusCallApiCourses = (state) => state.courses.selectStatusCallApiCourses;
 
 export default coursesSlice.reducer;
--- src/redux/tutors/tutors.js
+++ src/redux/tutors/tutors.js
@@ -1,63 +1,68 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  statusCallApiTutors: "idle",
-  tutors: [
-    // {
-    //   tutor_id: "1223442",
-    //   name: "Clint Eastwood",
-    //   tutor_sn: "sldkjfkp",
-    // },
-    // {
-    //   tutor_id: "12ju234442",
-    //   name: "Stephen Kind",
-    //   tutor_sn: "ncilkspom",
-    // },
-  ],
+  statusCallApiTutors: 'idle',
+  statusPostTutor: 'idle',
+  tutors: [],
+  repeatTutor: false,
 };
 
 export const fetchApiTutors = createAsyncThunk(
-  "tutors/fetchApiTutors",
+  'tutors/fetchApiTutors',
   async () => {
-    const response = await fetch("http://localhost:8000/tutors").then((res) =>
-      res.json()
-    );
+    const response = await fetch('http://localhost:8000/tutors').then((res) => res.json());
     return response;
-  }
+  },
+);
+
+export const fetchPostTutor = createAsyncThunk(
+  'tutor/fetchPostTutor',
+  async (tutor) => {
+    const response = await fetch('http://localhost:8000/tutors', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(tutor),
+    }).then((res) => res.json());
+    return response;
+  },
 );
 
 const tutorsSlice = createSlice({
-  name: "tutors",
+  name: 'tutors',
   initialState,
   reducers: {
     addTutor: (state, action) => {
       if (
         state.tutors.find((tutor) => tutor.tutor_sn === action.payload.tutor_sn)
       ) {
-        alert("Tutor already exist!");
-        return state;
+        alert('Tutor already exist!');
+        return {
+          ...state,
+          repeatTutor: true,
+        };
       }
       return {
         ...state,
         tutors: [...state.tutors, action.payload],
+        repeatTutor: false,
       };
     },
     editTutor: (state, action) => {
       // To not repeat the same tutor secure number
       if (
         state.tutors.find(
-          (tutor) =>
-            tutor.tutor_sn === action.payload.tutor_sn &&
-            tutor.tutor_id !== action.payload.tutor_id
+          (tutor) => tutor.tutor_sn === action.payload.tutor_sn
+            && tutor.tutor_id !== action.payload.tutor_id,
         )
       ) {
-        alert("Secure number already exist!");
+        alert('Secure number already exist!');
         return state;
       }
       const newTutors = state.tutors.map((tutor) => {
-        if (tutor.tutor_id === action.payload.tutor_id) {
+        if (tutor.id === action.payload.id) {
           return {
-            tutor_id: action.payload.tutor_id,
             name: action.payload.name,
             tutor_sn: action.payload.tutor_sn,
           };
@@ -71,7 +76,7 @@ const tutorsSlice = createSlice({
     },
     deleteTutor: (state, action) => {
       const newTutors = state.tutors.filter(
-        (tutor) => tutor.tutor_id !== action.payload
+        (tutor) => tutor.tutor_id !== action.payload,
       );
       return {
         ...state,
@@ -83,25 +88,37 @@ const tutorsSlice = createSlice({
     builder
       .addCase(fetchApiTutors.pending, (state) => ({
         ...state,
-        statusCallApiCourse: "loading",
+        statusCallApiTutors: 'loading',
       }))
       .addCase(fetchApiTutors.fulfilled, (state, action) => {
         const arrayTutors = action.payload.map((tutor) => ({
-          tutor_id: tutor.tutor_id,
           name: tutor.name,
+          tutor_sn: tutor.tutor_sn,
         }));
         return {
           ...state,
-          statusCallApiTutors: "done",
+          statusCallApiTutors: 'done',
           tutors: arrayTutors,
         };
-      });
+      })
+      .addCase(fetchPostTutor.pending, (state) => ({
+        ...state,
+        statusPostTutor: 'proccess',
+      }))
+      .addCase(fetchPostTutor.fulfilled, (state) => ({
+        ...state,
+        statusPostTutors: 'Create',
+      }))
+      .addCase(fetchPostTutor.rejected, (state, action) => ({
+        ...state,
+        statusPostTutor: action.payload,
+      }));
   },
 });
 
 export const { addTutor, editTutor, deleteTutor } = tutorsSlice.actions;
 export const selectTutors = (state) => state.tutors.tutors;
-export const selectStatusCallApiTutors = (state) =>
-  state.courses.selectStatusCallApiTutors;
+export const selectRepeatTutor = (state) => state.tutors.repeatTutor;
+export const selectStatusCallApiTutors = (state) => state.courses.selectStatusCallApiTutors;
 
 export default tutorsSlice.reducer;
