a2371d8408a13c60ea8b895ebc4a29103bd43d54
hendridg <hendridg@gmail.com>
2022-03-18 22:16:48 -0400

    Add pages and components files

---
+++ src/pages/Courses.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Courses = () => <div>Courses</div>;
+
+export default Courses;
---
+++ src/pages/Tutors.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Tutors = () => <div>Tutors</div>;
+
+export default Tutors;
