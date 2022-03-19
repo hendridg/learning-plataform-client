92ba3a6b507b9ac30f53773c82a086945e7f2f3b
hendridg <hendridg@gmail.com>
2022-03-19 18:32:15 -0400

    Add Button.js component

---
+++ src/components/Button.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Btn = styled.button`
+  display: block;
+  font-family: 'Poppins', sans-serif;
+  font-size: 0.8em;
+  padding: 1.5em 3em;
+  border-radius: 2em;
+  border: none;
+  background-color: greenyellow;
+  color: #1f1f1f;
+  background: ${(props) => (props.accent ? 'greenyellow' : '#228be6')};
+  font-weight: 500;
+  letter-spacing: 1px;
+  cursor: pointer;
+  transition: box-shadow ease-in-out 300ms, opacity ease-in-out 300ms;
+  :hover {
+    opacity: 0.9;
+    box-shadow: 0px 0px 0px 3px #fff,
+      0px 0px 0px 5px ${(props) => (props.accent ? 'greenyellow' : '#228be6')};
+  }
+`;
+
+const Button = ({ children, accent, clickBtn }) => (
+  <Btn accent={accent} onClick={clickBtn}>
+    {children}
+  </Btn>
+);
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  accent: PropTypes.bool,
+  clickBtn: PropTypes.func,
+};
+
+Button.defaultProps = {
+  accent: false,
+  clickBtn: () => {},
+};
+
+export default Button;
