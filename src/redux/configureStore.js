ea57143ddca070ff3145a2523866c5c5bb1b04fb
hendridg <hendridg@gmail.com>
2022-03-21 11:41:59 -0400

    Delete logger  middleware configurateStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducerCourses from './courses/courses';
 import reducerTutors from './tutors/tutors';
+import reducerStudents from './studens/students';
 
 const store = configureStore({
   reducer: {
     courses: reducerCourses,
     tutors: reducerTutors,
+    students: reducerStudents,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
