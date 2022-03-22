fc48bc4acfbf9862e497d44fef08f44a42ab6268
hendridg <hendridg@gmail.com>
2022-03-22 11:10:30 -0400

    Add select inputs to Querys.js component

--- src/pages/Querys.js
+++ src/pages/Querys.js
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchApiCourses, selectCourses } from '../redux/courses/courses';
+import { fetchApiTutors, selectTutors } from '../redux/tutors/tutors';
+import { fetchApiStudents, selectStudents } from '../redux/studens/students';
+import { Wrapper, Form, ContainerInputs } from '../components';
 
-const Querys = () => <div>Querys</div>;
+const Querys = () => {
+  const dispatch = useDispatch();
+  const courses = useSelector(selectCourses);
+  const tutors = useSelector(selectTutors);
+  const students = useSelector(selectStudents);
+
+  useEffect(() => {
+    dispatch(fetchApiCourses());
+    dispatch(fetchApiTutors());
+    dispatch(fetchApiStudents());
+  }, []);
+  return (
+    <Wrapper>
+      <h2>Querys</h2>
+      <Form>
+        <ContainerInputs>
+          <select>
+            <option>select course</option>
+            {courses.map((course) => (
+              <option key={course.id}>{course.name}</option>
+            ))}
+          </select>
+          <select>
+            <option>select tutor</option>
+            {tutors.map((tutor) => (
+              <option key={tutor.id}>{tutor.name}</option>
+            ))}
+          </select>
+          <select>
+            <option>select student</option>
+            {students.map((student) => (
+              <option key={student.id}>{student.name}</option>
+            ))}
+          </select>
+        </ContainerInputs>
+      </Form>
+    </Wrapper>
+  );
+};
 
 export default Querys;
