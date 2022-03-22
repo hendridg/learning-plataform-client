4b5d1e53cbf7e2b9f93afb8878c764c3bc2265af
hendridg <hendridg@gmail.com>
2022-03-22 11:09:46 -0400

    Add query reduxSlice to store in configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducerCourses from './courses/courses';
 import reducerTutors from './tutors/tutors';
 import reducerStudents from './studens/students';
+import reducerQuerys from './querys/querys';
 
 const store = configureStore({
   reducer: {
     courses: reducerCourses,
     tutors: reducerTutors,
     students: reducerStudents,
+    querys: reducerQuerys,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
